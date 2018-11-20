6f23fd90b8f760ba5dad4ff4cfe0ce1751c54962
Cristi Scorta <cristi.scorta@yahoo.ro>
2018-11-20 20:03:05 +0200

    Initial commit

---
+++ src/oop/Child.java
@@ -0,0 +1,15 @@
+package oop;
+
+public class Child extends Person {
+    public Child(String name, Integer energy) {
+        super(name, energy);
+    }
+    public void eat(){
+        energy++;
+    }
+
+    @Override
+    public void sleep() {
+        energy = 50;
+    }
+}
---
+++ src/oop/Person.java
@@ -0,0 +1,34 @@
+package oop;
+
+public class Person {
+
+    protected String name;
+    protected Integer energy;
+    protected Boolean alive = true;
+
+    public Person(String name, Integer energy) {
+        this.name = name;
+        this.energy = energy;
+    }
+
+    public void run() {
+        if (!alive) {
+            return;
+        }
+        energy -= 30;
+        if (energy <= 0)
+            alive = false;
+    }
+
+    public void sleep() {
+        if (!alive) {
+            return;
+        }
+        energy = 100;
+    }
+
+
+    public void showEnergy() {
+        System.out.println(name + " energy: " + energy);
+    }
+}
